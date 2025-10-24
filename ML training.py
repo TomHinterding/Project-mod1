908f79f7d66e471f73fd312403b449c97229fa2c
TomHinterding <tomwolf6h@gmail.com>
2025-10-24 23:43:08 +0200

    smol change

--- ML training.py
+++ ML training.py
@@ -8,24 +8,26 @@ import joblib
 table_dir = os.path.join("data/", "table/", "dataset_56_vote.csv")
 Table = pd.read_csv(table_dir)
 
+# only if we run the python fiel directly the ML model gets trained, this avoids retraining the model everytime we need
+# to acces something else in this file e.g. the function to acess the saved ML model
 
-#cleaning the Table
-vote_map = {'y': 1, 'n': -1, '?': 0}
-Table = Table.map(lambda v: vote_map.get(v, v))
-#split table into train and test data
-Train_x, Test_x, Train_y, Test_y = train_test_split(Table.drop('Class', axis = 1), Table['Class'], test_size = 0.2, random_state = 42)
-
-#create ML model
-knn_class = KNeighborsClassifier(n_neighbors=5)
-knn_class.fit(Train_x, Train_y)
-prediction = knn_class.predict(Test_x)
-acc = accuracy_score(Test_y, prediction)
-prec = precision_score(Test_y, prediction, pos_label='democrat')
-rec = recall_score(Test_y, prediction, pos_label='democrat')
-f1 = f1_score(Test_y, prediction, pos_label='democrat')
-
-#evaluate model
 if __name__ == '__main__':
+    #cleaning the Table
+    vote_map = {'y': 1, 'n': -1, '?': 0}
+    Table = Table.map(lambda v: vote_map.get(v, v))
+    #split table into train and test data
+    Train_x, Test_x, Train_y, Test_y = train_test_split(Table.drop('Class', axis = 1), Table['Class'], test_size = 0.2, random_state = 42)
+
+    #create ML model
+    knn_class = KNeighborsClassifier(n_neighbors=5)
+    knn_class.fit(Train_x, Train_y)
+    prediction = knn_class.predict(Test_x)
+    acc = accuracy_score(Test_y, prediction)
+    prec = precision_score(Test_y, prediction, pos_label='democrat')
+    rec = recall_score(Test_y, prediction, pos_label='democrat')
+    f1 = f1_score(Test_y, prediction, pos_label='democrat')
+
+    #evaluate model
     #we determine if it has no/a negative impact or a positive impact on the model if we leave out a feature when training a moddel
     negative_impact = []
     pOrNo_impact = []
@@ -34,12 +36,13 @@ if __name__ == '__main__':
         Train_x2, Test_x2, Train_y2, Test_y2 = train_test_split(Table.drop('Class', axis = 1).drop(col, axis=1), Table['Class'], test_size = 0.2, random_state = 42)
         knn_compare.fit(Train_x2, Train_y)
         comp_prediction = knn_compare.predict(Test_x2)
-        #acc_comp = accuracy_score(Test_y, comp_prediction)
+        acc_comp = accuracy_score(Test_y, comp_prediction)
         #prec_comp = precision_score(Test_y, comp_prediction, pos_label='democrat')
         #rec_comp = recall_score(Test_y, comp_prediction, pos_label='democrat')
         f1_comp = f1_score(Test_y, comp_prediction, pos_label='democrat')
-        diff = f1 - f1_comp
-        if diff > 0:
+        diffacc = acc - acc_comp
+        difff1 = f1 - f1_comp
+        if diffacc > 0 and difff1 > 0:
             negative_impact.append(col)
         else : 
             pOrNo_impact.append(col)
