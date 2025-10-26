2b7d3dcf8d0b9a5680550c401e2735f24a583685
TomHinterding <tomwolf6h@gmail.com>
2025-10-26 20:07:03 +0100

    added query stuff used a different sql query though

--- pages/Check Voting Patterns.py
+++ pages/Check Voting Patterns.py
@@ -1,45 +1,56 @@
 import streamlit as st
-
+import query as q
 st.set_page_config(
         page_title="Check Voting Patterns",
         layout="wide",
 )
 
+#selectable features for the multiselect / selectionbox box. Key is whatis saved when selecting, Value is what is shown to the user
+feature_options = {
+        "handicapped-infants" : "handicapped infants",
+        "water-project-cost-sharing" : "water project cost sharing",
+        "adoption-of-the-budget-resolution" : "adoption of the budget resolution",
+        "physician-fee-freeze" : "physician fee freeze",
+        "el-salvador-aid" : "el salvador aid",
+        "religious-groups-in-schools" : "religious groups in schools",
+        "anti-satellite-test-ban" : "anti satellite test ban",
+        "aid-to-nicaraguan-contras" : "aid to nicaraguan contras",
+        "mx-missile" : "mx missile",
+        "immigration" : "immigration",
+        "synfuels-corporation-cutback" : "synfuels corporation cutback",
+        "education-spending" : "education spending",
+        "superfund-right-to-sue" : "superfund right to sue",
+        "crime" : "crime",
+        "duty-free-exports" : "duty free exports",
+        "export-administration-act-south-africa" : "export administration act south africa"
+        }
+
 st.header("Check Voting Patterns")
 st.write("Select an issue to show voting statistics")
 st.divider()
 
+#creates a the selection session state variable that way we can update the selection directly
 if "selection" not in st.session_state:
     st.session_state.selection = "handicapped-infants"
 
+#adjusts size of multiselect
 col1, col2 = st.columns([3,10])
 with col1:
-        st.session_state.selection = st.selectbox("Select Your Issue:",
-                ['handicapped infants',
-                'water project cost sharing',
-                'adoption of the-budget resolution',
-                'physician fee freeze',
-                'el salvador aid',
-                'religious groups in schools',
-                'anti satellite test ban',
-                'aid to nicaraguan contras',
-                'mx missile',
-                'immigration',
-                'synfuels corporation cutback',
-                'education spending',
-                'superfund right to sue',
-                'crime',
-                'duty free exports',
-                'export administration act south africa'])
-
+        #if you want to change multiselect to a normal selection box, you just also need to change q.querySelectedfeatures function to query feature.
+        #might be needed if there is no way to neatly display the data of multiple features in a diagramm.
+        st.session_state.selection = st.multiselect("Select Your Issue:",list(feature_options), format_func=lambda x: feature_options[x], placeholder="Select an option...")
 st.divider()
 
-with st.container(border=True):
-        st.write("You Picked: ", st.session_state.selection)
-        st.write("Todo: Display Table")
+if len(st.session_state.selection) != 0:
 
-st.divider()
+        #Table container
+        with st.container(border=True):
+                st.write("You Picked: ", st.session_state.selection)
+                q = q.querySelectedfeatures(st.session_state.selection)
+                st.dataframe(q)
 
-with st.container(border=True):
-        st.write("You Picked: ", st.session_state.selection)
-        st.write("Todo: Display Graph")
+        st.divider()
+        #Diagram container
+        with st.container(border=True):
+                st.write("You Picked: ", st.session_state.selection)
+                st.write("Todo: Display Graph")
---
+++ query.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import pandasql as ps
+import os
+
+csv_path = os.path.join("data/", "table/", "dataset_56_vote.csv")
+Data = pd.read_csv(csv_path)
+def query(sql):
+    return ps.sqldf(sql, globals())
+#query feature is probably needed, everything else maybe not depending on how we want to proceed with the diagrams
+def queryfeature(feature):
+        q = query(f"""
+        SELECT df.Party,
+        df.feature,
+        df."?count",
+        df.ncount,
+        df.ycount,
+        ROUND(100 * (CAST(df."?count" AS FLOAT) / (df."?count" + df.ycount + df.ncount)), 2) as "?%",
+        ROUND(100 * (CAST(df.ncount AS FLOAT) / (df."?count" + df.ycount + df.ncount)), 2) as "n%",
+        ROUND(100 * (CAST(df.ycount AS FLOAT) / (df."?count" + df.ycount + df.ncount)), 2) as "y%"
+        FROM (SELECT d.Class AS Party, '{feature}' AS feature,
+        COUNT(CASE WHEN d."{feature}" = '?' THEN d."{feature}" END) AS "?count",
+        COUNT(CASE WHEN d."{feature}" = 'n' THEN d."{feature}" END) AS ycount,
+        COUNT(CASE WHEN d."{feature}" = 'y' THEN d."{feature}" END) AS ncount
+        FROM Data d
+        GROUP BY d.Class) df
+        GROUP BY df.Party
+        ORDER BY df.Party ASC;
+        """)
+        return q
+
+def querySelectedfeatures(featureList):
+    selectionquery = pd.DataFrame()
+    for col in featureList:
+          selectionquery = pd.concat([selectionquery, queryfeature(col)], ignore_index=True)
+    return selectionquery 
+
+def queryall():
+    usedData = Data.drop("Class", axis=1)
+    allfeatureList = usedData.columns.tolist()
+    totalquery = querySelectedfeatures(allfeatureList)
+    return totalquery
+    
+
+print(queryfeature("handicapped-infants"))
+print(queryall())
